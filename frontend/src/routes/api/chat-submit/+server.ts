8193950ddf80d19be9999c4ad34b227d3d024550
townsag <townsag@gmail.com>
2024-05-08 12:49:46 -0700

    added functionality to show errors on unsuccessful newMessage or unsuccessful load of previous messages

--- frontend/src/routes/api/chat-submit/+server.ts
+++ frontend/src/routes/api/chat-submit/+server.ts
@@ -15,7 +15,6 @@ interface ResponseData {
 
 export async function POST(event: RequestEvent): Promise<Response> {
 	const { conv_id, conv_offset, content }: RequestData = await event.request.json();
-
 	try {
 		const response = await fetch('http://127.0.0.1:5000/chat/newMessage', {
 			method: 'POST',
@@ -30,20 +29,32 @@ export async function POST(event: RequestEvent): Promise<Response> {
 			})
 		});
         // ToDo: add some error handling behavior here if the flask app fails
-
-		const data = await response.json();
-		const responseData: ResponseData = {
-			conv_id: conv_id,
-			conv_offset: conv_offset + 1,
-			sender_role: 'assistant',
-			content: data.content
-		};
-		return new Response(JSON.stringify(responseData), {
-			status: 200,
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
+        if (response.ok){
+            const data = await response.json();
+            const responseData: ResponseData = {
+                conv_id: conv_id,
+                conv_offset: conv_offset + 1,
+                sender_role: 'assistant',
+                content: data.content
+            };
+            return new Response(JSON.stringify(responseData), {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        } else {
+            return new Response(
+                JSON.stringify({"msg":"backend server error"}),
+                {
+                    status:500,
+                    headers:{
+                        'Content-Type':'application/json'
+                    }
+                }
+            );
+        }
+		
 	} catch (error) {
 		return new Response(JSON.stringify({ msg: 'an error has occured' }), {
 			status: 500,
